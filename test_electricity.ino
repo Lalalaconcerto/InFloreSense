18ad37a74622930847d114a91a4e2b539880d0a1
August Weinbren <weinbrenaugust@gmail.com>
2022-02-21 12:27:22 +0000

    Capturing user touch, first version
    Touch voltage value (experimental)

--- test_electricity.ino
+++ test_electricity.ino
@@ -4,6 +4,7 @@ int counter;
 int baseCounter = 1;
 int baseVoltage;
 int baseVoltageSum = 0;
+bool userTouch = false;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
@@ -25,6 +26,9 @@ else if (baseCounter == 20) {
 else {
   voltage_val = analogRead(leafPin);
   Serial.println(voltage_val);
+  if (voltage_val > 400) { // This is the value we found experimentally to be consistent with massaging a plant leaf
+    userTouch = true;
+  }
 }
 delay(100);
   // put your main code here, to run repeatedly:
